3d9518ac3408d8d88523ea0fa6378c41cedfd29f
Brent <BrentHickman@ymail.com>
2023-02-02 15:06:39 -0800

    fight function accepts chosen character and monster.
    Co-authored-by: NicksFed <nick.guzy76@gmail.com>

--- src/fight.js
+++ src/fight.js
@@ -16,4 +16,5 @@ export default function fight(character, monster) {
     result = "you lost";
   }
   return result;
+  
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import fight from './../src/fight.js';
 import Monster from './../src/Monster.js';
 import Character from './../src/Character.js';
 
+
 //display selected 
 function showChar(myCharacter){
   let newChar = document.createElement("li");
@@ -43,60 +44,77 @@ function showMonst(myEnemy){
   document.getElementById("showMonst").append(newMonstAtk);
 }
 
-
+// fight
+function startFight(myCharacter, myEnemy){
+  fight(myCharacter, myEnemy);
+  console.log("result");
+}
 
 //character select section
-function charSubmit(event){
-  event.preventDefault();
+function charSubmit(){
   let choseType =  document.querySelector(".charBuilder select option:checked").value;
-  console.log(choseType);
   if (choseType === "warrior") {
     let myCharacter = new Character("warrior", "strength", 5, 5);
     showChar(myCharacter);
+    return myCharacter;
   }
   else if (choseType === "mage"){
     let myCharacter = new Character("mage", "wisdom", 3, 7);
     showChar(myCharacter);
+    return myCharacter;
   }
   else if (choseType === "rogue"){
     let myCharacter = new Character("rogue", "intellect", 4, 6);
     showChar(myCharacter);
+    return myCharacter;
   }
   else {
     console.log("PLEASE CHOOSE A CHARACTER!");
   }
 }
 
-function monstSubmit(event){
-  event.preventDefault(event);
+function monstSubmit(){
   let choseMonster =  document.querySelector(".monsterForm select option:checked").value;
   console.log(choseMonster);
   if (choseMonster === "goblin") {
     let myEnemy = new Monster("Goblin", 5, 2);
     showMonst(myEnemy);
+    return myEnemy;
   }
   else if (choseMonster === "troll"){
     let myEnemy = new Monster("Troll", 10, 3);
     showMonst(myEnemy);
+    return myEnemy;
   }
   else if (choseMonster === "demon"){
     let myEnemy = new Monster("Demon", 15, 5);
     showMonst(myEnemy);
+    return myEnemy;
   }
   else {
     console.log("PLEASE CHOOSE A MONSTER!");
   }
 }
 
+
 window.addEventListener("load", () => {
   const charForm = document.getElementById("charBuilder");
   const monstForm = document.getElementById("monsterForm");
+  const fightBtn = document.getElementById("fight");
+  let myCharacter;
+  let myEnemy;
+
   charForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    charSubmit(event);
+    myCharacter = charSubmit();
   });
   monstForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    monstSubmit(event);
+    myEnemy = monstSubmit();
   });
+  fightBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    startFight(myCharacter, myEnemy);
+  });
+
 });
