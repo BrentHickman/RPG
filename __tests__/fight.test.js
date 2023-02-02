2f10b22e164980117b9b5ad98f71b58c4d961ede
Brent <Brenthickman@ymail.com>
2023-02-01 16:25:11 -0800

    wrote test 3 for fight(). Good fail
    Co-authored-by: NicksFed <nick.guzy76@gmail.com>

--- __tests__/fight.test.js
+++ __tests__/fight.test.js
@@ -6,8 +6,6 @@ import Character from './../src/Character.js';
 describe('fight', () => {
 
   test('should correctly return lost message', () => {
-    const myCharacter = new Character("warrior", "strength", 5, 5);
-    const monster1 = new Monster("Monster1", 10, 1);
     let result = "";
     expect(fight()).toEqual("you lost");
   });
@@ -15,6 +13,13 @@ describe('fight', () => {
   test('should correctly return you won message', () => {
     const myCharacter = new Character("warrior", "strength", 5, 5);
     const monster1 = new Monster("Monster1", 0, 1);
+    let result = "";
+    expect(fight(myCharacter, monster1)).toEqual("you won");
+  });
+
+  test('should subtract character attack from monster hp and vice versa until monster hit points = 0', () => {
+    const myCharacter = new Character("warrior", "strength", 5, 5);
+    const monster1 = new Monster("Monster1", 10, 1);
     let result = "";
     expect(fight(myCharacter, monster1)).toEqual("you won");
   });
--- src/fight.js
+++ src/fight.js
@@ -1,5 +1,10 @@
 export default function fight(character, monster) {
   let result = "";
+  // for (let i = 0; i < monster.hitPoints; i++){
+  //   monster.hitPoints - character.attack;
+  //   character.hitPoints - monster.attack;
+  // }
+
   if (monster.hitPoints === 0 && character.hitPoints > 0){
     result = "you won";
   }
